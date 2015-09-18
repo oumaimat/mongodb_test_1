1c93921889ca51f181bfe8e646ef02c73055a9c1
oumaimat <turki.oumaima@gmail.com>
2015-09-18 23:34:35 +0200

    Seperate GenericDAO class

---
+++ DAO/GenericDAO.py
@@ -0,0 +1,72 @@
+__author__ = 'OTurki'
+
+import pymongo
+
+
+class ConnectionToDatabase :
+
+    db_conn=None
+    database = None
+
+    #penser a creer un pool de connections reutilisables vers la base
+    #solution actuelle non envisageable en prod
+
+    def __init__(self):
+        self.connectToDatabase()
+
+
+    def connectToDatabase(self) :
+        # Connexion au serveur de Mongo DB
+
+        try:
+            # db_conn=pymongo.MongoClient("mongodb://admin:3usRy1SmJ8gH@127.0.0.1:37391/")
+            db_conn=pymongo.MongoClient()
+            print("Connected successfully!!!")
+            print(db_conn)
+        except pymongo.errors.ConnectionFailure :
+            print("Could not connect to MongoDB: %s")
+
+
+        # Connexion a la base du projet
+        #db = db_conn["mongodbtest0"]
+        db = db_conn["GPSLocationDB"]
+
+
+        #Initialisation des variables globales
+        ConnectionToDatabase.database = db
+
+
+    def getCollection(self, collection_name):
+
+        #Connexion a la collection
+        collection = ConnectionToDatabase.database[collection_name]
+
+        #function result
+        return collection
+
+class GenericDAO :
+
+    # Mettre a jour un enregistrement dans une collection et inserer si pas d'enregistrement trouve
+    def updateObjects(self, collection, objectCriteria, objectUpdate):
+
+        updateResult = collection.update(objectCriteria, objectUpdate, upsert = True)
+
+        return updateResult
+
+    # Extraire les livreurs les plus proches d'une localisation dans un rayon donné
+    def getObjects(self, collection, userCoordinates):
+
+        locationDict = {"location" :
+                            {"$near": {
+                              "$geometry" : {
+                                  "type" : "Point",
+                                  "coordinates" : userCoordinates
+                              }  ,
+                                "$maxDistance" : 500
+                            }
+                             }
+                        }
+
+        foundObjects = list(collection.find(locationDict, limit=10))
+
+        return foundObjects
--- manageLocation.py
+++ manageLocation.py
@@ -1,77 +1,9 @@
 __author__ = 'OTurki'
 
-import pymongo
+from DAO.GenericDAO import GenericDAO, ConnectionToDatabase
 from threading import Thread
 from pubnub import Pubnub
 import os
-
-class ConnectionToDatabase :
-
-    db_conn=None
-    database = None
-
-    #penser a creer un pool de connections reutilisables vers la base
-    #solution actuelle non envisageable en prod
-
-    def __init__(self):
-        self.connectToDatabase()
-
-
-    def connectToDatabase(self) :
-        # Connexion au serveur de Mongo DB
-
-        try:
-            # db_conn=pymongo.MongoClient("mongodb://admin:3usRy1SmJ8gH@127.0.0.1:37391/")
-            db_conn=pymongo.MongoClient()
-            print("Connected successfully!!!")
-            print(db_conn)
-        except pymongo.errors.ConnectionFailure :
-            print("Could not connect to MongoDB: %s")
-
-
-        # Connexion a la base du projet
-        #db = db_conn["mongodbtest0"]
-        db = db_conn["GPSLocationDB"]
-
-
-        #Initialisation des variables globales
-        ConnectionToDatabase.database = db
-
-
-    def getCollection(self, collection_name):
-
-        #Connexion a la collection
-        collection = ConnectionToDatabase.database[collection_name]
-
-        #function result
-        return collection
-
-class GenericDAO :
-
-    # Mettre a jour un enregistrement dans une collection et inserer si pas d'enregistrement trouve
-    def updateObjects(self, collection, objectCriteria, objectUpdate):
-
-        updateResult = collection.update(objectCriteria, objectUpdate, upsert = True)
-
-        return updateResult
-
-    # Extraire les livreurs les plus proches d'une localisation dans un rayon donné
-    def getObjects(self, collection, userCoordinates):
-
-        locationDict = {"location" :
-                            {"$near": {
-                              "$geometry" : {
-                                  "type" : "Point",
-                                  "coordinates" : userCoordinates
-                              }  ,
-                                "$maxDistance" : 500
-                            }
-                             }
-                        }
-
-        foundObjects = list(collection.find(locationDict, limit=10))
-
-        return foundObjects
 
 class manageGPSLocation(Thread) :
 
@@ -94,7 +26,7 @@ class manageGPSLocation(Thread) :
         print(message)
         criteriaDict = {}
         criteriaDict["_id"]=message["_id"]
-        
+
         updateDict = {}
         subUpdateDict = {}
 
