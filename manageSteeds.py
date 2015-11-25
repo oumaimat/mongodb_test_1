55a599bb4a223e9a823dce90ad8e6d6ef115f7c6
oumaimat <turki.oumaima@gmail.com>
2015-11-25 17:18:06 +0100

    Clients & Steeds in progress

--- manageSteeds.py
+++ manageSteeds.py
@@ -20,13 +20,18 @@ class manageSteeds(Thread) :
     def __init__(self):
         Thread.__init__(self)
         mongodb_connection = ConnectionToDatabase()
-        manageSteeds.steeds_collection = mongodb_connection.getCollection("available_steeds")
-        manageSteeds.deliveries_collection = mongodb_connection.getCollection("temp_deliveries")
+
+        self.steeds_collection = mongodb_connection.getCollection("available_steeds")
+        self.deliveries_collection = mongodb_connection.getCollection("temp_deliveries")
+        self.deliveries_steeds_collection = mongodb_connection.getCollection("temp_deliveries_steeds")
+
         self.pubnub_settings = Pubnub(publish_key=manageSteeds.pubnub_publish_key,subscribe_key=manageSteeds.pubnub_subscribe_key)
         # Rename to location channel
         self.pubnub_channel = "steeds_channel"
+
         self.genericDAO = GenericDAO()
-        self.sceduler = sched.scheduler() # how to instansiate a scheduler
+
+        self.scheduler = sched.scheduler() # Instansiate a scheduler
 
     def subscriber_callback(self, message, channel):
         x = 1
@@ -55,7 +60,7 @@ class manageSteeds(Thread) :
 
         # se desinscire du channel
         self.pubnub_settings.unsubscribe(self.pubnub_channel)
-
+# how to instansiate a scheduler
 
 
     #Out of Pubnub functions
@@ -74,22 +79,59 @@ class manageSteeds(Thread) :
         # Get nearest steeds to pickup location
         nearest_steeds = self.genericDAO.getNearestSteeds(self, self.steeds_collection, client_coordinates)
 
-        import sched
+        # Save available steeds for delivery
+        delivery_steeds = {}
+        delivery_steeds["iddelivery"] = iddelivery
+        delivery_steeds["available_steeds"] = nearest_steeds
+
+        self.genericDAO.insertObject(self.deliveries_steeds_collection, delivery_steeds)
+
+        #Send delivery request to seeder
+        self.sendDeliveryRequestToSteed(iddelivery)
+
+    def sendDeliveryRequestToSteed(self, iddelivery):
+
+        # Search delivery by id (criteria)
+        delivery_criteria = {}
+        delivery_criteria["_id"] = iddelivery
+
+        temp_delivery = self.genericDAO.getOneObject(self, self.deliveries_colllection, delivery_criteria)
+
+        #Check received_positive_response field
+        received_response = temp_delivery["received_positive_response"]
+
+        if(not(received_response)) :
+
+            #Search available steeds for delivery
+            delivery_steeds_criteria = {}
+            delivery_steeds_criteria["iddelivery"] = iddelivery
+
+            available_steeds = self.genericDAO.getOneObject(self, self.deliveries_steeds_collection, delivery_steeds_criteria)
 
 
+            #Send steed delivery request
+            self.publish("XXXX"+iddelivery+available_steeds[0]["_id"]+" "+"Other delivery details")
 
-    def sendDeliveryRequestToSeeder(self, available_seeders, iddelivery):
-        self.publish("XXXX test message")
+            #Delete 1st steed from available steeds list
+            new_available_steeds = available_steeds[1:len(available_steeds)-1]
 
-        scheduler.enter(2, 1, print_event)
-        scheduler.enter(3, 1, print_event)
+            #Update delivery available steeds
 
-        scheduler.run()
+            update_search_criteria = {}
+            update_search_criteria["iddelivery"] = iddelivery
 
-    def sendRequestsToSteeds(self,nearestSteed):
+            update_field = {}
+            update_field["available_steeds"] = new_available_steeds
 
-        steedRequest = {}
-        steedRequest["msg_code"] = "00"
+
+            #Add update function to Generic DAO from old projects
+
+
+            #Schedule method call
+            self.scheduler.enter(10, 1, self.sendDeliveryRequestToSteed(iddelivery) )
+            self.scheduler.run()
+
+
 
 
 
